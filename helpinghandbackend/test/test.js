ec0caa4790a37ee01a71307ac5af3eced358cf56
Akhil-J <akhil.jonnalagadda@gmail.com>
2021-05-16 10:59:24 +0530

    test and logger added along with npm mocha and supertest and winston

---
+++ helpinghandbackend/test/test.js
@@ -0,0 +1,31 @@
+const app = require("../app");
+//const logger = require("../logger");
+const request = require("supertest");
+let token;
+const baseUrl = "http://localhost:8000/api/Signup";
+// Signup;
+describe("\n\n\n\nSignup ::", () => {
+    it("Failed to signup \n\n", (done) => {
+        request(baseUrl)
+            .post("/")
+            .send({
+                name: "akhilj",
+                email: "akhil@gmail.com",
+                password: "a123456",
+            })
+            .end((err, res) => {
+                if (res.body.name==="akhilj" ) {
+                    console.log("success");
+                    //logger.error(err);
+                    //throw err;
+                }
+                console.log(req.body);
+                if (res.body.name!=="akhilj") {
+                    console.log("not valid");
+                    logger.info(res.body);
+                    token = res.body.token;
+                }
+                done();
+            });
+    });
+});
